6f56bf54790f42da6c16dafb5c23ef069b1bcc95
Chris Wald <wald.j.chris@gmail.com>
2017-11-18 14:06:24 -0600

    Fix page title

--- createuser.php
+++ createuser.php
@@ -18,7 +18,7 @@
 
 <html>
     <head>
-        <title>Login</title>
+        <title>Sign up</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
         <link rel="stylesheet" href="./style/login.css" media="screen">
         <link href="https://fonts.googleapis.com/css?family=Roboto+Mono" rel="stylesheet">
